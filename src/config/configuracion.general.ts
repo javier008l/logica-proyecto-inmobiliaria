d211bb08c06284ffb60dca513fd1b315828a8759
Eliana <eliana04112001@gmail.com>
2023-04-14 10:22:31 -0500

    Eliminación de variables anteriores del Formulario de contacto

--- src/config/configuracion.general.ts
+++ src/config/configuracion.general.ts
@@ -2,8 +2,4 @@ export namespace ConfiguracionGeneral {
   export const carpetaArchivosInmuebles: string = "../../archivos/inmuebles";
   export const campoDeInmueble: string = "file";
   export const extensionesImagenes: string[] = [".jpg", ".PNG", ".JPEG"];
-  export const urlNotificaciones2fa =
-    'http://localhost:5154/Notificaciones/enviar-correo-2fa';
-  export const correoAdministradorContacto: string = "leon55490@gmail.com";
-  export const administradorNombreContacto: string = "eliana";
 }
