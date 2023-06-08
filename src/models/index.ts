3e118eb168ebeb927ad1e63b43d5d69a90cc3d2b
Eliana <eliana04112001@gmail.com>
2023-06-08 09:55:55 -0500

    Ajustes

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
+export * from './asesor-id.model';
 export * from './asesor.model';
 export * from './ciudad.model';
 export * from './cliente.model';
@@ -10,15 +11,11 @@ export * from './estado.model';
 export * from './formulario-asesor.model';
 export * from './formulario-contacto.model';
 export * from './inmueble.model';
+export * from './nombre-departamento.model';
+export * from './paginador-cliente.model';
 export * from './solicitud.model';
+export * from './solicitudes-cliente.model';
 export * from './tipo-inmueble.model';
 export * from './tipo-solicitud.model';
 export * from './variables-generales-del-sistema.model';
 
-export * from './solicitudes-cliente.model';
-export * from './asesor-id.model';
-<<<<<<< HEAD
-export * from './paginador-cliente.model';
-=======
-export * from './nombre-departamento.model';
->>>>>>> 8d919c9998fb3d5ed93f24e0c9a79af6787a177a
